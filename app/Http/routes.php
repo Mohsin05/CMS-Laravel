af6a31f0300da43f83fc98da253daf699d5ba630
mohsin05 <mohsinyounas05@gmail.com>
2018-04-22 14:23:19 +0200

    Admin master page setup

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,7 +14,22 @@
 Route::get('/', function () {
     return view('welcome');
 });
-
 Route::auth();
-
 Route::get('/home', 'HomeController@index');
+
+
+
+Route::get('/admin', function () {
+    return view('admin.index');
+});
+
+
+
+
+
+Route::resource('admin/users','AdminUsersController');
+
+
+
+
+
